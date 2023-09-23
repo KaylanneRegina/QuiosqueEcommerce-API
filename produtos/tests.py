8d943ef43f2bdbb396e559dfcf9ceb811884a39b
KayyLanne <kaylanne.regina@Hotmail.com>
2023-09-23 02:45:04 -0300

    add tests

--- produtos/tests.py
+++ produtos/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from produtos.models import Categoria
 
-# Create your tests here.
+@pytest.mark.django_db
+
+def test_categoria_create():
+    Categoria.objects.create(nome='Categoria A')
+    assert Categoria.objects.count() == 1
+    
